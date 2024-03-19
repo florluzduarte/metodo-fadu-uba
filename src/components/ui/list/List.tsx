e668931bfb4c47ce5aa5eb3f28e492cbb4d9ec9a
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-03-19 16:22:15 -0300

    fix: change List and ListItem tags for better accesibility

--- src/components/ui/list/List.tsx
+++ src/components/ui/list/List.tsx
@@ -3,12 +3,12 @@ import { ListItem } from "@/components";
 
 export const List = ({ listItems }: ListProps) => {
   return (
-    <div className="flex flex-col gap-6">
+    <ul className="flex flex-col gap-6">
       {
         listItems.map((listItem) => (
           <ListItem key={listItem.order} {...listItem} />
         ))
       }
-    </div>
+    </ul>
   )
 }
